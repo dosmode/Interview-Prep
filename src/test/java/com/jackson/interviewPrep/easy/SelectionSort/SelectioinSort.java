3760be0eaac6c9f9aff91814e8ca709b87460e04
jackson <dosmode111@gmail.com>
2021-12-17 15:09:37 +0900

    Dec 17, 2021

---
+++ src/test/java/com/jackson/interviewPrep/easy/SelectionSort/SelectioinSort.java
@@ -0,0 +1,40 @@
+package com.jackson.interviewPrep.easy.SelectionSort;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TournamentWinner {
+    @Test
+    public void TestCase1() {
+        int[] expected = {2, 3, 5, 5, 6, 8, 9};
+        int[] input = {8, 5, 2, 9, 5, 6, 3};
+        assertTrue(compare(Program.selectionSort(input), expected));
+    }
+
+    public boolean compare(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Program {
+    public static int[] selectionSort(int[] array) {
+        // Write your code here.
+
+        return  Arrays.stream(array).sorted().toArray();
+    }
+}
+
---
+++ src/test/java/com/jackson/interviewPrep/easy/Template/Template.java
@@ -0,0 +1,12 @@
+package com.jackson.interviewPrep.easy.Template;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Template {
+
+}
+
+class Program {
+
+}
+
