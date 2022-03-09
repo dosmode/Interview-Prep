bfe0e86802cb53f555a3b10c929cd3d16b99a804
Jungsik Jackson Byun <dosmode111@gmail.com>
2022-03-08 21:33:09 -0600

    03_08_2022

--- src/test/java/com/jackson/algoExpert/Arrays/Easy/MinimumWaitingTime/ValidateSubsequence/MinimumWaitingTime.java
+++ src/test/java/com/jackson/algoExpert/Arrays/Easy/MinimumWaitingTime/ValidateSubsequence/MinimumWaitingTime.java
@@ -15,14 +15,21 @@ class ProgramTest {
 	public void TestCase1() {
 		int[] queries = new int[] {3, 2, 1, 2, 6};
 		int expected = 17;
-		var actual = new Program().minimumWaitingTime(queries);
+		var actual = new com.jackson.algoExpert.Arrays.Easy.MinimumWaitingTime.ValidateSubsequence.MinimumWaitingTime().minimumWaitingTime(queries);
 		assertTrue(actual == expected);
 	}
 }
 
-class Program {
+class MinimumWaitingTime {
 	public int minimumWaitingTime(int[] queries) {
-		// Write your code here.
-		return -1;
+		Arrays.sort(queries);
+		int result = 0;
+		int counter = 0;
+		for(int i  : queries){
+
+			result += i * ( queries.length - counter -1 );
+			counter++;
+		}
+		return result;
 	}
 }
