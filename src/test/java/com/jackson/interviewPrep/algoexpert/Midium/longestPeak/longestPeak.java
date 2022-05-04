580951414760db77e24c59e704e6b7ec1a56ab86
Jungsik Jackson Byun <dosmode111@gmail.com>
2022-05-03 21:52:47 -0500

    05/03/2022

---
+++ src/test/java/com/jackson/interviewPrep/algoexpert/Midium/longestPeak/longestPeak.java
@@ -0,0 +1,49 @@
+package com.jackson.interviewPrep.algoexpert.Midium.longestPeak;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class ProgramTest {
+	@Test
+	public void TestCase1() {
+		var input = new int[] {1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3};
+		var expected = 6;
+		var actual = Program.longestPeak(input);
+		assertTrue(expected == actual);
+	}
+}
+
+
+class Program {
+	public static int longestPeak(int[] array) {
+		if(array.length<3) return 0;
+		int result =0;
+
+		int increasing;
+		int decreasing;
+		for (int i = 1; i < array.length - 1; i++) {
+			increasing = 0;
+			decreasing = 0;
+			int base = i;
+			while (base <  array.length && array[base-1] < array[base]) {
+				base++;
+				increasing++;
+			}
+			while (base < array.length && array[base-1] > array[base] && increasing >0) {
+				base++;
+				decreasing++;
+			}
+			if (increasing > 0 && decreasing > 0) {
+				result = Math.max(result, increasing + decreasing + 1);
+			}
+		}
+		return result;
+	}
+}
